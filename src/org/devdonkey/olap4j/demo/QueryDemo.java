ed6f29927e12f04dd64741398c0ddeebad35de2d
lucboudreau <lucboudreau@32ac81a3-ab02-0ec2-17db-e8c2d81a26af>
2010-09-16 18:58:42 +0000

    Initial import.

---
+++ src/org/devdonkey/olap4j/demo/QueryDemo.java
@@ -0,0 +1,46 @@
+package org.devdonkey.olap4j.demo;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.olap4j.CellSet;
+import org.olap4j.OlapConnection;
+import org.olap4j.layout.RectangularCellSetFormatter;
+
+public class QueryDemo {
+
+    public static void main(String[] args) throws Exception {
+        
+        final String query = "SELECT { Except( {[Product].[All Products].[Drink].[Beverages].Children}, {[Product].[All Products].[Drink].[Beverages].[Carbonated Beverages]} ) } ON COLUMNS, { [Store].[All Stores].[USA].Children } ON ROWS FROM [Sales] WHERE ([Time].[1997])";
+        
+        // Load the driver
+        Class.forName("mondrian.olap4j.MondrianOlap4jDriver");
+        
+        // Connect
+        final Connection connection =
+            DriverManager.getConnection(
+            "jdbc:mondrian:"                                                            // Driver ident
+            + "JdbcDrivers=com.mysql.jdbc.Driver;"                                      // Relational driver
+//            + "Jdbc=jdbc:mysql://localhost/foodmart?user=foodmart&password=foodmart;"   // Relational DB
+            + "Jdbc=jdbc:hsqldb:file:foodmart/foodmart;"                                // Relational DB
+            + "Catalog=file:foodmart/FoodMart.xml;");                                   // Catalog
+        
+     // We are dealing with an olap connection. we must unwrap it.
+        final OlapConnection olapConnection = connection.unwrap(OlapConnection.class);
+
+        // Prepare a statement.
+        final CellSet cellSet = olapConnection
+            .createStatement()              // Prepare a statement
+                .executeOlapQuery(query);   // Execute some query
+
+        // We use the utility formatter.
+        RectangularCellSetFormatter formatter =
+            new RectangularCellSetFormatter(false);
+
+        // Print out.
+        PrintWriter writer = new PrintWriter(System.out);
+        formatter.format(cellSet, writer);
+        writer.flush();
+    }
+}
