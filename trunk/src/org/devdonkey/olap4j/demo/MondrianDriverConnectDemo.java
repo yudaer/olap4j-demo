59217ed156343563f2ba75cd2e31b48d6039d077
lucboudreau <lucboudreau@32ac81a3-ab02-0ec2-17db-e8c2d81a26af>
2010-09-16 18:58:42 +0000

    Initial import.

---
+++ trunk/src/org/devdonkey/olap4j/demo/MondrianDriverConnectDemo.java
@@ -0,0 +1,39 @@
+package org.devdonkey.olap4j.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.olap4j.OlapConnection;
+
+public class MondrianDriverConnectDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        // Load the driver
+        Class.forName("mondrian.olap4j.MondrianOlap4jDriver");
+        
+        // Connect
+        final Connection connection =
+            DriverManager.getConnection(
+            "jdbc:mondrian:"                                                            // Driver ident
+            + "JdbcDrivers=com.mysql.jdbc.Driver;"                                      // Relational driver
+//            + "Jdbc=jdbc:mysql://localhost/foodmart?user=foodmart&password=foodmart;"   // Relational DB
+            + "Jdbc=jdbc:hsqldb:file:foodmart/foodmart;"                                // Relational DB
+            + "Catalog=file:foodmart/FoodMart.xml;");                                   // Catalog
+        
+        // We are dealing with an olap connection. we must unwrap it.
+        final OlapConnection olapConnection = connection.unwrap(OlapConnection.class);
+
+        // Check if it's all groovy
+        System.out.println(
+                olapConnection.getMetaData().getDriverName()
+                + " -> "
+                + olapConnection.getMetaData().getDatabaseProductName()
+                + " version " + olapConnection.getMetaData().getDatabaseMajorVersion()
+                + "." + olapConnection.getMetaData().getDatabaseMinorVersion());
+        
+        // Done
+        connection.close();
+    }
+
+}
